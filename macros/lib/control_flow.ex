9fb8bc6a140637bedb00f217f5164720e1fe95b8
akthrms <akt.hrms@gmail.com>
2021-05-13 06:35:19 +0900

    update

--- macros/lib/control_flow.ex
+++ macros/lib/control_flow.ex
@@ -4,4 +4,17 @@ defmodule ControlFlow do
       if !unquote(expression), do: unquote(block)
     end
   end
+
+  defmacro my_if(expression, do: if_block) do
+    if(expression, do: if_block, else: nil)
+  end
+
+  defmacro my_if(expression, do: if_block, else: else_block) do
+    quote do
+      case unquote(expression) do
+        result when result in [false, nil] -> unquote(else_block)
+        _ -> unquote(if_block)
+      end
+    end
+  end
 end
